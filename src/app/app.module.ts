011aa515569db2313ff267ec9ea7b1d8cfcd527c
Julie J <jeannet.julie@gmail.com>
2021-12-21 15:59:19 +0100

    new search-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
