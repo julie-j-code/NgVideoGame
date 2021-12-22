5a8f2da5339f881e58d2b023fd329e80b6c132d3
Julie J <jeannet.julie@gmail.com>
2021-12-22 06:43:04 +0100

    interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { GaugeModule } from "angular-gauge";
 import { MatTabsModule } from "@angular/material/tabs";
@@ -16,6 +16,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpHeadersInterceptor } from './interceptors/src/app/interceptors/http-headers.interceptor';
+import { HttpParamsInterceptor } from './interceptors/src/app/interceptors/http.params.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/src/app/interceptors/http-errors.interceptor';
 
 @NgModule({
   declarations: [
@@ -36,7 +39,23 @@ import { HomeComponent } from './components/home/home.component';
     GaugeModule.forRoot()
 
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpParamsInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
